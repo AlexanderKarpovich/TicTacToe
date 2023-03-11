c22e614024b0e1ca6e3f64c55d446da4de8b6940
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-11 20:13:18 +0300

    Game cell model class created and implemented.

---
+++ TicTacToeGame/Models/GameCell.cs
@@ -0,0 +1,28 @@
+﻿namespace TicTacToeGame.Models
+{
+    public class GameCell
+    {
+        public GameCell() { }
+
+        public GameCell(int number)
+        {
+            Position = number;
+        }
+
+        public int Position { get; }
+        public GameVariant Variant { get; private set; } = GameVariant.Clear;
+
+        public int GameSessionId { get; set; }
+
+        public bool MakeMove(GameVariant variant)
+        {
+            if (Variant == GameVariant.Clear)
+            {
+                Variant = variant;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
