adaaf740ef5afc7d0085ef88e153e834220b7b77
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-11 20:09:21 +0300

    Identity database context added.

---
+++ TicTacToeGame/Data/AppIdentityDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TicTacToeGame.Models;
+
+namespace TicTacToeGame.Data
+{
+    public class AppIdentityDbContext : IdentityDbContext<GameUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options) { }
+    }
+}
