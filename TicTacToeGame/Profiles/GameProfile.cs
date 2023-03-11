d5bc4f166f7f2b507986623d1ca588ce1a8adb15
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-11 20:21:28 +0300

    Automapper profile for game added.

---
+++ TicTacToeGame/Profiles/GameProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TicTacToeGame.Dtos;
+using TicTacToeGame.Models;
+
+namespace TicTacToeGame.Profiles
+{
+    public class GameProfile : Profile
+    {
+        public GameProfile()
+        {
+            // Source --> Target
+            CreateMap<GameSession, GameSessionReadDto>();
+        }
+    }
+}
