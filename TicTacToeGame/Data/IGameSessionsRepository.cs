d6537c6f771bbb9ead4701bc7aec6cf0660c38b4
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-11 20:19:33 +0300

    Game sessions repository interface added.

---
+++ TicTacToeGame/Data/IGameSessionsRepository.cs
@@ -0,0 +1,20 @@
+﻿using TicTacToeGame.Models;
+
+namespace TicTacToeGame.Data
+{
+    public interface IGameSessionsRepository
+    {
+        void AddGameSession(GameSession session);
+        IEnumerable<GameSession> GetGameSessions();
+        IEnumerable<GameSession> GetOldGameSessions(TimeSpan lifeInterval);
+        GameSession? GetGameSessionById(int id);
+        void UpdateGameSession(GameSession session);
+        void RemoveGameSession(int id);
+
+        bool IsPlayerAlreadyInGame(string playerName);
+        bool IsPlayerInGameSession(int gameSessionId, string playerName);
+        Player? GetPlayerByName(string playerName);
+
+        bool SaveChanges();
+    }
+}
