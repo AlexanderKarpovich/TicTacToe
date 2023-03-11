4c87b48d5904cefcbee5887b21c5fa8e1e425344
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-11 20:21:08 +0300

    Read dto for game session added.

---
+++ TicTacToeGame/Dtos/GameSessionReadDto.cs
@@ -0,0 +1,14 @@
+﻿using TicTacToeGame.Models;
+
+namespace TicTacToeGame.Dtos
+{
+    public class GameSessionReadDto
+    {
+        public int GameSessionId { get; set; }
+        public Player? Player1 { get; set; }
+        public Player? Player2 { get; set; }
+        public Player? PlayerTurn { get; set; }
+        public Player? Winner { get; set; }
+        public IEnumerable<GameCell> Cells { get; set; } = default!;
+    }
+}
