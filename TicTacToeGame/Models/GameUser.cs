40bef769c691d9e9767993db7b0b3df37242da15
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-11 20:11:32 +0300

    Game user model added.

---
+++ TicTacToeGame/Models/GameUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TicTacToeGame.Models
+{
+    public class GameUser : IdentityUser
+    {
+        public GameUser(string userName) : base(userName) { }
+    }
+}
