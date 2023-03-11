84fb497e2486c9d31a0475e923ffd54871adae01
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-11 20:10:46 +0300

    Сreated a class to populate the identity database.

---
+++ TicTacToeGame/Data/SeedIdentityData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TicTacToeGame.Models;
+
+namespace TicTacToeGame.Data
+{
+    public class SeedIdentityData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Secret123$";
+
+        public static async Task EnsurePopulated(IApplicationBuilder app)
+        {
+            AppIdentityDbContext context = app.ApplicationServices.CreateScope()
+                .ServiceProvider.GetRequiredService<AppIdentityDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            UserManager<GameUser> userManager = app.ApplicationServices.CreateScope()
+                .ServiceProvider.GetRequiredService<UserManager<GameUser>>();
+
+            GameUser? user = await userManager.FindByIdAsync(adminUser);
+            if (user == null)
+            {
+                user = new GameUser("Admin")
+                {
+                    Email = "admin@example.com",
+                    PhoneNumber = "555-1234"
+                };
+
+                await userManager.CreateAsync(user, adminPassword);
+            }
+        }
+    }
+}
