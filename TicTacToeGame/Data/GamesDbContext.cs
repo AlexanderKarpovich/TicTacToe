ecde8212781fd9e855c0f8610fbdbefbd474af17
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-11 20:19:07 +0300

    Database context for games added.

---
+++ TicTacToeGame/Data/GamesDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicTacToeGame.Data.EntityConfigurations;
+using TicTacToeGame.Models;
+
+namespace TicTacToeGame.Data
+{
+    public class GamesDbContext : DbContext
+    {
+        public GamesDbContext(DbContextOptions<GamesDbContext> options) : base(options) { }
+
+        public DbSet<GameSession> GameSessions { get; set; } = default!;
+        public DbSet<Player> Players { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new GameSessionEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new GameCellEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new PlayerEntityTypeConfiguration());
+        }
+    }
+}
