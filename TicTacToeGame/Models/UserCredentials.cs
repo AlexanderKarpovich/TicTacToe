5bfb45172bddae18ac7d193734723c174f2b929d
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-11 20:11:48 +0300

    User credentials model class added.

---
+++ TicTacToeGame/Models/UserCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace TicTacToeGame.Models
+{
+    public class UserCredentials
+    {
+        public string UserName { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
