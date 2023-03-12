41d76d1fc74a95f337a0ce89081442fe86db5168
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-12 16:57:01 +0300

    Global usings updated.

--- TicTacToe.UnitTests/GlobalUsings.cs
+++ TicTacToe.UnitTests/GlobalUsings.cs
@@ -1,3 +1,13 @@
-﻿global using Xunit;
+﻿global using Moq;
+global using Xunit;
+
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+
+global using System.Linq;
+global using System.Threading.Tasks;
+global using System.Collections.Generic;
 
 global using TicTacToeGame.Models;
+global using TicTacToeGame.Controllers;
