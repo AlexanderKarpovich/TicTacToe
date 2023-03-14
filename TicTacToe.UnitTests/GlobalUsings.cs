288331caff03507608d2a917165c8a2a859129e9
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-14 15:12:13 +0300

    Global usings file updated.

--- TicTacToe.UnitTests/GlobalUsings.cs
+++ TicTacToe.UnitTests/GlobalUsings.cs
@@ -1,6 +1,7 @@
 ﻿global using Moq;
 global using Xunit;
 global using AutoMapper;
+global using Grpc.Core;
 
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.AspNetCore.Http;
@@ -9,15 +10,20 @@ global using Microsoft.AspNetCore.SignalR;
 
 global using System;
 global using System.Linq;
+global using System.Threading;
 global using System.Threading.Tasks;
 global using System.Collections.Generic;
 global using System.Security.Principal;
 
+global using TicTacToe.Api;
 global using TicTacToe.Api.Models;
 global using TicTacToe.Api.Controllers;
 global using TicTacToe.Api.Data;
 global using TicTacToe.Api.Profiles;
 global using TicTacToe.Api.Dtos;
 global using TicTacToe.Api.SignalR;
+global using TicTacToe.Api.Services;
 
+global using TicTacToe.UnitTests.Profiles;
 global using TicTacToe.UnitTests.MoqServices;
+global using TicTacToe.UnitTests.Helpers;
